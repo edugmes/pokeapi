d271d55129f5fa718e5562a812808ac48770dc13
Eduardo Gomes <edu.gmes0@gmail.com>
2022-09-01 14:51:39 -0300

    feat (fastAPI): add call to retrieve berries basic info

--- project/main.py
+++ project/main.py
@@ -1,7 +1,26 @@
+import json
+
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def berries_basic_info(offset: int = 0, limit: int = 1) -> dict:
+    url = 'https://pokeapi.co/api/v2/berry/'
+    params = {'offset': offset, 'limit': limit}
+
+    client = httpx.Client()
+
+    response = client.get(url, params=params)
+    data = json.loads(response.text)
+
+    return data
+
+def berries_count(berries_general_info: dict) -> int:
+    return berries_general_info['count']
+
+
 @app.get("/")
 def read_root():
-    return {"Message": "Initial project structure"}
+    data = berries_basic_info()
+    return {"berries_count": berries_count(data), 'berries_data': data}
