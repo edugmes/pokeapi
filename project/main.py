324c320e8739355528b96ab91e6c3848aaf6d2af
Eduardo Gomes <edu.gmes0@gmail.com>
2022-09-01 21:38:34 -0300

    fix (docs): format code with autopep8

--- project/main.py
+++ project/main.py
@@ -15,10 +15,9 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup() -> None:
-    """Setup redis connection to cache requests
-    """
+    """Setup redis connection to cache requests"""
     redis_addr = os.getenv("REDIS_URL")
-    redis =  aioredis.from_url(redis_addr, encoding="utf8", decode_responses=True)
+    redis = aioredis.from_url(redis_addr, encoding="utf8", decode_responses=True)  # type: ignore
 
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
@@ -30,7 +29,7 @@ async def berry_info(berry_id: int, http_client: httpx.AsyncClient) -> dict:
     :param http_client: A httpx.Client object to use for the request
     :return: A dictionary with the berry info
     """
-    url = f'https://pokeapi.co/api/v2/berry/{berry_id}/'
+    url = f"https://pokeapi.co/api/v2/berry/{berry_id}/"
 
     response = await http_client.get(url)
 
@@ -46,8 +45,8 @@ def berries_basic_info(offset: int = 0, limit: int = 1) -> dict:
     :param limit: The number of results to return, defaults to 1
     :return: A dictionary with the list of berries API information
     """
-    url = 'https://pokeapi.co/api/v2/berry/'
-    params = {'offset': offset, 'limit': limit}
+    url = "https://pokeapi.co/api/v2/berry/"
+    params = {"offset": offset, "limit": limit}
 
     client = httpx.Client()
 
@@ -63,7 +62,7 @@ def berries_count(berries_general_info: dict) -> int:
     :param berries_general_info: The dictionary with berries general info
     :return: Number of berries
     """
-    return berries_general_info['count']
+    return berries_general_info["count"]
 
 
 async def berries_specific_info(count: int) -> list:
@@ -75,10 +74,13 @@ async def berries_specific_info(count: int) -> list:
     result = []
 
     async with httpx.AsyncClient() as http_client:
-        berries = [berry_info(berry_id, http_client) for berry_id in range(1, count+1)]
+        berries = [
+            berry_info(berry_id, http_client) for berry_id in range(1, count + 1)
+        ]
         result = await gather(*berries)
 
     return result
+
 
 def filter_specific_info(specific_info: list, filter: list = []) -> pd.DataFrame:
     """Given a list of dictionaries of berries specific data, return a dataframe with the columns specified in the filter list
@@ -98,29 +100,31 @@ def filter_specific_info(specific_info: list, filter: list = []) -> pd.DataFrame
 
     return df
 
+
 def berries_stats(berries_df: pd.DataFrame) -> dict:
     """Generate growth time stats for berries
 
     :param berries_df: A pandas dataframe with the berries names and growth time
     :return: A dictionary with berries names and growth stats (min, max, median, variance, mean, and frequency)
     """
-    names = berries_df['name'].tolist()
-    min_value = int(berries_df['growth_time'].min())
-    median_value = round(float(berries_df['growth_time'].median()), 2)
-    max_value = int(berries_df['growth_time'].max())
-    variance_value = round(float(berries_df['growth_time'].var()), 2)
-    mean_value = round(float(berries_df['growth_time'].mean()), 2)
-    frequency = berries_df['growth_time'].value_counts().to_dict()
+    names = berries_df["name"].tolist()
+    min_value = int(berries_df["growth_time"].min())
+    median_value = round(float(berries_df["growth_time"].median()), 2)
+    max_value = int(berries_df["growth_time"].max())
+    variance_value = round(float(berries_df["growth_time"].var()), 2)  # type: ignore
+    mean_value = round(float(berries_df["growth_time"].mean()), 2)
+    frequency = berries_df["growth_time"].value_counts().to_dict()
 
     return {
-        'berries_names': names,
-        'min_growth_time': min_value,
-        'median_growth_time': median_value,
-        'max_growth_time': max_value,
-        'variance_growth_time': variance_value,
-        'mean_growth_time': mean_value,
-        'frequency_growth_time': frequency,
+        "berries_names": names,
+        "min_growth_time": min_value,
+        "median_growth_time": median_value,
+        "max_growth_time": max_value,
+        "variance_growth_time": variance_value,
+        "mean_growth_time": mean_value,
+        "frequency_growth_time": frequency,
     }
+
 
 @app.get("/allBerryStats")
 @cache(expire=60)
@@ -129,7 +133,7 @@ async def all_berry_stats():
     count_info = berries_count(basic_info)
     specific_info = await berries_specific_info(count_info)
 
-    df = filter_specific_info(specific_info, filter=['name', 'growth_time'])
+    df = filter_specific_info(specific_info, filter=["name", "growth_time"])
 
     stats = berries_stats(df)
 
